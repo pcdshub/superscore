9436ce94ff815da4306ccc5ba1f2b50d85fbe6ab
tangkong <tangkongrobert@gmail.com>
2024-02-21 13:14:20 -0800

    Initial commit from cookiecutter-pcds-python

---
+++ README.rst
@@ -0,0 +1,32 @@
+===============================
+superscore
+===============================
+
+.. image:: https://github.com/pcdshub/superscore/actions/workflows/standard.yml/badge.svg
+        :target: https://github.com/pcdshub/superscore/actions/workflows/standard.yml
+
+.. image:: https://img.shields.io/pypi/v/superscore.svg
+        :target: https://pypi.python.org/pypi/superscore
+
+
+`Documentation <https://pcdshub.github.io/superscore/>`_
+
+Configuration Management for EPICS PVs
+
+Requirements
+------------
+
+* Python 3.9+
+
+Installation
+------------
+
+::
+
+  $ pip install .
+
+Running the Tests
+-----------------
+::
+
+  $ pytest -v
---
+++ superscore/bin/main.py
@@ -0,0 +1,103 @@
+"""
+`superscore` is the top-level command for accessing various subcommands.
+
+Try:
+
+"""
+
+import argparse
+import asyncio
+import importlib
+import logging
+from inspect import iscoroutinefunction
+
+import superscore
+
+DESCRIPTION = __doc__
+
+
+MODULES = ("help", )
+
+
+def _try_import(module):
+    relative_module = f'.{module}'
+    return importlib.import_module(relative_module, 'superscore.bin')
+
+
+def _build_commands():
+    global DESCRIPTION
+    result = {}
+    unavailable = []
+
+    for module in sorted(MODULES):
+        try:
+            mod = _try_import(module)
+        except Exception as ex:
+            unavailable.append((module, ex))
+        else:
+            result[module] = (mod.build_arg_parser, mod.main)
+            DESCRIPTION += f'\n    $ superscore {module} --help'
+
+    if unavailable:
+        DESCRIPTION += '\n\n'
+
+        for module, ex in unavailable:
+            DESCRIPTION += (
+                f'\nWARNING: "superscore {module}" is unavailable due to:'
+                f'\n\t{ex.__class__.__name__}: {ex}'
+            )
+
+    return result
+
+
+COMMANDS = _build_commands()
+
+
+def main():
+    top_parser = argparse.ArgumentParser(
+        prog='superscore',
+        description=DESCRIPTION,
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    top_parser.add_argument(
+        '--version', '-V',
+        action='version',
+        version=superscore.__version__,
+        help="Show the superscore version number and exit."
+    )
+
+    top_parser.add_argument(
+        '--log', '-l', dest='log_level',
+        default='INFO',
+        type=str,
+        help='Python logging level (e.g. DEBUG, INFO, WARNING)'
+    )
+
+    subparsers = top_parser.add_subparsers(help='Possible subcommands')
+    for command_name, (build_func, main) in COMMANDS.items():
+        sub = subparsers.add_parser(command_name)
+        build_func(sub)
+        sub.set_defaults(func=main)
+
+    args = top_parser.parse_args()
+    kwargs = vars(args)
+    log_level = kwargs.pop('log_level')
+
+    logger = logging.getLogger('superscore')
+    logger.setLevel(log_level)
+    logging.basicConfig()
+
+    if hasattr(args, 'func'):
+        func = kwargs.pop('func')
+        logger.debug('%s(**%r)', func.__name__, kwargs)
+        if iscoroutinefunction(func):
+            asyncio.run(func(**kwargs))
+        else:
+            func(**kwargs)
+    else:
+        top_parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
