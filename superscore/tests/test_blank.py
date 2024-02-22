df57a73d1bcd06f6fe80625bb5f1ec0291e634cf
tangkong <tangkongrobert@gmail.com>
2024-02-22 12:03:20 -0800

    TST: add a real test

--- superscore/tests/test_blank.py
+++
@@ -1,8 +0,0 @@
-def test_blank():
-    """
-    This is a placeholder for some testing and to verify your continuous
-    integration works as expected.
-
-    Remove this function and write your own tests!
-    """
-    raise ZeroDivisionError()
