f8e5586643dc577990152ebb5f1da2e7c4e2710b
Devan Agrawal <devagr@slac.stanford.edu>
2024-06-04 11:45:15 -0700

    TST: remove linac_backend dependency in TestEntryValidation

--- superscore/tests/test_model.py
+++ superscore/tests/test_model.py
@@ -1,7 +1,7 @@
 import apischema
 
-from superscore.model import (Collection, Parameter, Root, Setpoint, Severity,
-                              Snapshot, Status)
+from superscore.model import (Collection, Parameter, Readback, Root, Setpoint,
+                              Severity, Snapshot, Status)
 
 
 def test_serialize_collection_roundtrip():
@@ -46,56 +46,110 @@ def test_sample_database_roundtrip(sample_database: Root):
 
 class TestEntryValidation:
     @staticmethod
+    def test_parameter_readback_validation():
+        readback_pv = Parameter(
+            pv_name="TEST:PV:READBACK",
+            description="Used as a readback for another PV",
+        )
+        main_pv = Parameter(
+            pv_name="TEST:PV:MAIN",
+            description="A PV with a separate readback",
+            readback=readback_pv,
+        )
+        assert readback_pv.validate()
+        assert main_pv.validate()
+
+        readback_pv.abs_tolerance = "1"
+        assert not main_pv.validate()
+
+    @staticmethod
+    def test_setpoint_validation():
+        readback = Readback(
+            pv_name="TEST:PV:READBACK",
+            description="Used as a readback for another PV",
+            data=0.0,
+        )
+        setpoint = Setpoint(
+            pv_name="TEST:PV:SETPOINT",
+            description="A PV with a separate readback",
+            data=5.0,
+            readback=readback,
+            creation_time=readback.creation_time,
+        )
+        assert readback.validate()
+        assert setpoint.validate()
+
+        readback.rel_tolerance = "10%"
+        assert not setpoint.validate()
+
+    @staticmethod
+    def test_nestable_empty_validation():
+        empty_col = Collection(
+            title="Emtpy Collection",
+            description="A Collection without any children",
+            children=[],
+        )
+        assert not empty_col.validate()
+
+        parent_col = Collection(
+            title="Non-empty Collection",
+            description="A Collection whose child is empty",
+            children=[empty_col],
+        )
+        assert not parent_col.validate()
+
+    @staticmethod
+    def test_nestable_cycle_validation():
+        pv = Parameter(
+            pv_name="TEST:PV:1",
+            description="An ordinary, valid PV",
+        )
+        col = Collection(
+            title="Collection",
+            description="A valid Collection that becomes invalid when it becomes its own parent",
+            children=[pv]
+        )
+        assert col.validate()
+
+        col.children.append(col)
+        assert not col.validate()
+
+        # ensure there's no hysteresis in cycle detection
+        col.children.remove(col)
+        assert col.validate()
+
+    @staticmethod
+    def test_collection_reachable_from_snapshot_validation():
+        pv = Parameter(
+            pv_name="TEST:PV:1",
+            description="An ordinary, valid PV",
+        )
+        col = Collection(
+            title="Collection",
+            description="An ordinary, valid Collection",
+            children=[pv]
+        )
+        assert col.validate()
+
+        setpoint = Setpoint(
+            pv_name=pv.pv_name,
+            description=pv.description,
+            data=0,
+        )
+        snapshot = Snapshot(
+            title="Snapshot",
+            description="A valid Snapshot that becomes invalid when it gains a child Collection",
+            children=[setpoint]
+        )
+        assert snapshot.validate()
+
+        snapshot.children.append(col)
+        assert not snapshot.validate()
+
+    @staticmethod
     def test_fixture_validation(linac_backend):
         linac_model = linac_backend.get_entry("441ff79f-4948-480e-9646-55a1462a5a70")
         assert linac_model.validate()
 
         linac_snapshot = linac_backend.get_entry("06282731-33ea-4270-ba14-098872e627dc")
         assert linac_snapshot.validate()
-
-    @staticmethod
-    def test_parameter_readback_validation(linac_backend):
-        vac_gunb_pv1 = linac_backend.get_entry("8f3ac401-68f8-4def-b65a-3c8116c80ba7")
-        vac_gunb_pv2 = linac_backend.get_entry("06448272-cd38-4bb4-9b8d-292673a497e9")
-        vac_gunb_pv1.readback = vac_gunb_pv2
-        assert vac_gunb_pv1.validate()
-
-        vac_gunb_pv2.abs_tolerance = "1"
-        assert not vac_gunb_pv1.validate()
-
-    @staticmethod
-    def test_setpoint_validation(linac_backend):
-        vac_li21_readback = linac_backend.get_entry("de66d08e-09c3-4c45-8978-900e51d00248")
-        vac_li21_setpoint = linac_backend.get_entry("4bffe9a5-f198-41d8-90ab-870d1b5a325b")
-        assert vac_li21_readback.validate()
-        assert vac_li21_setpoint.validate()
-
-        vac_li21_readback.rel_tolerance = "10%"
-        assert not vac_li21_setpoint.validate()
-
-    @staticmethod
-    def test_nestable_empty_validation(linac_backend):
-        linac_model = linac_backend.get_entry("441ff79f-4948-480e-9646-55a1462a5a70")
-        vac_bsy_col = linac_backend.get_entry("22c2d597-2139-4c02-ac86-27f474728fad")
-        vac_bsy_col.children = []
-        assert not linac_model.validate()
-
-    @staticmethod
-    def test_nestable_cycle_validation(linac_backend):
-        linac_model = linac_backend.get_entry("441ff79f-4948-480e-9646-55a1462a5a70")
-        bsy_col = linac_backend.get_entry("2506d87a-5980-4470-b29a-63eea183f53d")
-        vac_bsy_col = linac_backend.get_entry("22c2d597-2139-4c02-ac86-27f474728fad")
-        vac_bsy_col.children.append(bsy_col)
-        assert not linac_model.validate()
-
-        # ensure there's no hysteresis in cycle detection
-        vac_bsy_col.children.remove(bsy_col)
-        assert linac_model.validate()
-
-    @staticmethod
-    def test_collection_reachable_from_snapshot_validation(linac_backend):
-        linac_snapshot = linac_backend.get_entry("06282731-33ea-4270-ba14-098872e627dc")
-        bsy_snapshot = linac_backend.get_entry("a62f1386-39de-4d19-9ea4-82f0212169a7")
-        vac_bsy_col = linac_backend.get_entry("22c2d597-2139-4c02-ac86-27f474728fad")
-        bsy_snapshot.children.append(vac_bsy_col)
-        assert not linac_snapshot.validate()
