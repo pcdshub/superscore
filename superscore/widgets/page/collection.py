62ab67290a185edf5ff1fbc40b76bc33d73f26e5
tangkong <tangkongrobert@gmail.com>
2024-06-21 12:43:46 -0700

    GUI/WIP: basic CollectionPage, mostly to serve as an example

---
+++ superscore/widgets/page/collection.py
@@ -0,0 +1,30 @@
+
+from qtpy import QtWidgets
+
+from superscore.model import Collection
+from superscore.widgets.core import DataWidget, Display, NameDescTagsWidget
+from superscore.widgets.manip_helpers import insert_widget
+
+
+class CollectionPage(Display, DataWidget):
+    filename = 'collection_page.ui'
+
+    meta_placeholder: QtWidgets.QWidget
+    meta_widget: NameDescTagsWidget
+    child_table: QtWidgets.QTreeView
+    pv_table: QtWidgets.QTableWidget
+    repr_text_edit: QtWidgets.QTextEdit
+
+    data: Collection
+
+    def __init__(self, *args, data: Collection, **kwargs):
+        super().__init__(*args, data=data, **kwargs)
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.meta_widget = NameDescTagsWidget(data=self.data)
+        insert_widget(self.meta_widget, self.meta_placeholder)
+
+        self.repr_text_edit.setText(str(self.data))
+        # recurse through children and gather PVs
+        # show tree view
