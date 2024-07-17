d44d15724c9e1ec41871a6bbdc0e8fa12852ca70
tangkong <tangkongrobert@gmail.com>
2024-07-17 15:15:04 -0700

    GUI/WIP: add table-view model and open button delegate

--- superscore/widgets/page/search.py
+++ superscore/widgets/page/search.py
@@ -1,13 +1,15 @@
 """Search page"""
 
 import logging
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from dateutil import tz
+from PyQt5.QtCore import QModelIndex
+from PyQt5.QtWidgets import QStyleOptionViewItem, QWidget
 from qtpy import QtCore, QtWidgets
 
 from superscore.client import Client
-from superscore.model import Collection, Readback, Setpoint, Snapshot
+from superscore.model import Collection, Entry, Readback, Setpoint, Snapshot
 from superscore.widgets.core import Display
 
 logger = logging.getLogger(__name__)
@@ -41,6 +43,7 @@ class SearchPage(Display, QtWidgets.QWidget):
     def __init__(self, *args, client: Client, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.client = client
+        self.model: Optional[ResultModel] = None
 
         self.type_checkboxes: List[QtWidgets.QCheckBox] = [
             self.snapshot_checkbox, self.collection_checkbox,
@@ -51,7 +54,16 @@ class SearchPage(Display, QtWidgets.QWidget):
     def setup_ui(self) -> None:
         self.start_dt_edit.setDate(QtCore.QDate.currentDate().addDays(-365))
         self.end_dt_edit.setDate(QtCore.QDate.currentDate())
-        self.apply_filter_button.clicked.connect(self._gather_search_terms)
+        self.apply_filter_button.clicked.connect(self.show_current_filter)
+
+        self.model = ResultModel(entries=[])
+        self.results_table_view.setModel(self.model)
+        horiz_header = self.results_table_view.horizontalHeader()
+        horiz_header.setSectionResizeMode(horiz_header.Interactive)
+
+        self.open_delegate = ButtonDelegate(button_text='open me')
+        del_col = len(ResultModel.headers) - 1
+        self.results_table_view.setItemDelegateForColumn(del_col, self.open_delegate)
 
     def _gather_search_terms(self) -> Dict[str, Any]:
         search_kwargs = {}
@@ -91,3 +103,131 @@ class SearchPage(Display, QtWidgets.QWidget):
 
         logger.debug(f'gathered search terms: {search_kwargs}')
         return search_kwargs
+
+    def show_current_filter(self) -> None:
+        # gather filter details
+        search_kwargs = self._gather_search_terms()
+        entries = self.client.search(**search_kwargs)
+
+        # create table
+        self.model = ResultModel(entries=list(entries))
+        self.results_table_view.setModel(self.model)
+
+
+class ResultModel(QtCore.QAbstractTableModel):
+    headers: List[str] = ['Name', 'Type', 'Description', 'Created', 'Open']
+
+    def __init__(self, *args, entries: List[Entry] = None, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.entries: List[Entry] = entries or []
+
+    def data(self, index: QtCore.QModelIndex, role: int) -> Any:
+        """
+        Returns the data stored under the given role for the item
+        referred to by the index.
+
+        Parameters
+        ----------
+        index : QtCore.QModelIndex
+            An index referring to a cell of the TableView
+        role : int
+            The requested data role.
+
+        Returns
+        -------
+        Any
+            the requested data
+        """
+        entry: Entry = self.entries[index.row()]
+
+        # Special handling for open button delegate
+        if index.column() == (len(self.headers) - 1):
+            if role in (QtCore.Qt.DisplayRole, QtCore.Qt.EditRole):
+                return 'open'
+
+        if role != QtCore.Qt.DisplayRole:
+            # table is read only
+            return QtCore.QVariant()
+
+        if index.column() == 0:  # name column, no edit permissions
+            name_text = getattr(entry, 'title', getattr(entry, 'pv_name', '<N/A>'))
+            return name_text
+        elif index.column() == 1:  # Type
+            return type(entry).__name__
+        elif index.column() == 2:  # Description
+            return getattr(entry, 'description', '<no desc>')
+        elif index.column() == 3:  # Creation time
+            return entry.creation_time.strftime('%Y/%m/%d %H:%M')
+
+        # if nothing is found, return invalid QVariant
+        return QtCore.QVariant()
+
+    def rowCount(self, index):
+        return len(self.entries)
+
+    def columnCount(self, index):
+        return len(self.headers)
+
+    def headerData(
+        self,
+        section: int,
+        orientation: QtCore.Qt.Orientation,
+        role: int
+    ) -> Any:
+        """
+        Returns the header data for the model.
+        Currently only displays horizontal header data
+        """
+        if role != QtCore.Qt.DisplayRole:
+            return
+
+        if orientation == QtCore.Qt.Horizontal:
+            return self.headers[section]
+
+    def flags(self, index: QtCore.QModelIndex) -> QtCore.Qt.ItemFlag:
+        """
+        Returns the item flags for the given ``index``.  The returned
+        item flag controls what behaviors the item supports.
+
+        Parameters
+        ----------
+        index : QtCore.QModelIndex
+            the index referring to a cell of the TableView
+
+        Returns
+        -------
+        QtCore.Qt.ItemFlag
+            the ItemFlag corresponding to the cell
+        """
+        if (index.column() == len(self.headers) - 1):
+            return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled
+        else:
+            return QtCore.Qt.ItemIsEnabled
+
+
+class ButtonDelegate(QtWidgets.QStyledItemDelegate):
+    clicked = QtCore.Signal(int)
+
+    def __init__(self, *args, button_text: str = '', **kwargs):
+        self.button_text = button_text
+        super().__init__(*args, **kwargs)
+
+    def createEditor(
+        self,
+        parent: QtWidgets.QWidget,
+        option,
+        index: QtCore.QModelIndex
+    ) -> QtWidgets.QWidget:
+        button = QtWidgets.QPushButton(self.button_text, parent)
+        button.clicked.connect(
+            lambda _, row=index.row(): self.clicked.emit(row)
+        )
+        return button
+
+    def updateEditorGeometry(
+        self,
+        editor: QWidget,
+        option: QStyleOptionViewItem,
+        index: QModelIndex
+    ) -> None:
+        return editor.setGeometry(option.rect)
