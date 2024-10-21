0db43da450a57b80e67da6a687d3b5a4e9c2dd0b
tangkong <tangkongrobert@gmail.com>
2024-10-21 14:15:16 -0700

    MNT: update page_map, Parameter Icon

--- superscore/widgets/page/__init__.py
+++ superscore/widgets/page/__init__.py
@@ -3,14 +3,15 @@ def get_page_map():
     from superscore.model import (Collection, Parameter, Readback, Setpoint,
                                   Snapshot)
     from superscore.widgets.page.entry import (CollectionPage, ParameterPage,
+                                               ReadbackPage, SetpointPage,
                                                SnapshotPage)
 
     page_map = {
         Collection: CollectionPage,
         Snapshot: SnapshotPage,
         Parameter: ParameterPage,
-        Setpoint: ParameterPage,
-        Readback: ParameterPage,
+        Setpoint: SetpointPage,
+        Readback: ReadbackPage,
     }
 
     return page_map
