0e5f6eb9ff0b009867e0dbaa80acca57d4070a83
tangkong <tangkongrobert@gmail.com>
2025-02-28 14:03:29 -0800

    MNT/DOC: improves clarity of subcommand files and adds some explanations (hopefully)

--- superscore/bin/main.py
+++ superscore/bin/main.py
@@ -26,13 +26,25 @@ def _try_import(module):
 
 
 def _build_commands():
+    """
+    Search for *_parser submodules to gather and build subcommands.
+    Each submodule should have two functions:
+    * `build_arg_parser`: takes an argparser instance and adds any necessary
+                          arguments
+    * `main`: imports the main routine from a different submodule and runs it
+
+    This separation ensures that heavier imports can be deferred until necessary,
+    and argparse details (help text, argument descriptions) are returned quickly.
+
+    Also builds up the the argparse description whenever a subcommand is added.
+    """
     global DESCRIPTION
     result = {}
     unavailable = []
 
     for module in sorted(MODULES):
         try:
-            mod = _try_import(module)
+            mod = _try_import(module + "_parser")
         except Exception as ex:
             unavailable.append((module, ex))
         else:
