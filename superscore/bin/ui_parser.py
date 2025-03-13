e4afaebd44c185eb20add18bfeb015326ab1fdb7
tangkong <tangkongrobert@gmail.com>
2025-03-13 11:31:17 -0700

    MNT: add descriptions to subcommand parsers, add option to provide config file path to superscore ui

--- superscore/bin/ui_parser.py
+++ superscore/bin/ui_parser.py
@@ -1,5 +1,8 @@
 """
-`superscore ui` opens up the main application window
+`superscore ui` opens up the main application window.
+
+The application will attempt to find and read configuration file.  This
+configuration file path can also be provided manually.
 """
 import argparse
 
@@ -7,7 +10,14 @@ import argparse
 def build_arg_parser(argparser=None):
     if argparser is None:
         argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "--cfg-path",
+        dest="cfg_path",
+        type=str,
+        help="A file path to a valid configuration file"
+    )
 
+    argparser.description = __doc__
     return argparser
 
 
