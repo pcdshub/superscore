ca6810f4b4526c38104c82993424f669aeeb6932
zdomke <zdomke@slac.stanford.edu>
2025-05-19 17:25:46 -0700

    DOC: Add typehinting to Page

--- superscore/widgets/page/page.py
+++ superscore/widgets/page/page.py
@@ -1,11 +1,13 @@
 from qtpy import QtWidgets
 from qtpy.QtGui import QCloseEvent
 
+from superscore.widgets.pv_table import PVTableModel
+
 
 class Page(QtWidgets.QWidget):
     def __init__(self, parent: QtWidgets.QWidget) -> None:
         super().__init__(parent)
-        self.live_models = set()
+        self.live_models: set[PVTableModel] = set()
 
     def closeEvent(self, a0: QCloseEvent) -> None:
         for model in self.live_models:
