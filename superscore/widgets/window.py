b0077a4e508db89b5b249bbf20e301b4660f1c8c
Alexander Ng <ang@slac.stanford.edu>
2025-05-23 15:36:22 -0700

    GUI: Partial implementation of populating PVDetails dataclass with real data

--- superscore/widgets/window.py
+++ superscore/widgets/window.py
@@ -13,14 +13,15 @@ from qtpy import QtCore, QtWidgets
 from qtpy.QtGui import QCloseEvent
 
 from superscore.client import Client
-from superscore.model import Snapshot
+from superscore.control_layers._base_shim import EpicsData
+from superscore.model import Parameter, Readback, Setpoint, Snapshot
 from superscore.widgets.core import NameDescTagsWidget, QtSingleton
 from superscore.widgets.page.page import Page
 from superscore.widgets.page.snapshot_comparison import SnapshotComparisonPage
 from superscore.widgets.page.snapshot_details import SnapshotDetailsPage
-from superscore.widgets.pv_browser_table import (PVBrowserFilterProxyModel,
-                                                 PVBrowserTableModel)
+from superscore.widgets.pv_browser_table import PVBrowserFilterProxyModel, PVBrowserTableModel
 from superscore.widgets.pv_details_components import PVDetails, PVDetailsPopup
+from superscore.widgets.pv_table import PVTableModel
 from superscore.widgets.snapshot_table import SnapshotTableModel
 from superscore.widgets.views import DiffDispatcher
 
@@ -216,9 +217,7 @@ class Window(QtWidgets.QMainWindow, metaclass=QtSingleton):
         metadata_dialog = QtWidgets.QDialog(parent=self)
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(NameDescTagsWidget(data=dest, tag_options=self.client.backend.get_tags()))
-        buttonBox = QtWidgets.QDialogButtonBox(
-            QtWidgets.QDialogButtonBox.Save | QtWidgets.QDialogButtonBox.Cancel
-        )
+        buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Save | QtWidgets.QDialogButtonBox.Cancel)
         layout.addWidget(buttonBox)
         buttonBox.accepted.connect(metadata_dialog.accept)
         buttonBox.rejected.connect(metadata_dialog.reject)
@@ -235,20 +234,33 @@ class Window(QtWidgets.QMainWindow, metaclass=QtSingleton):
 
     @QtCore.Slot(QtCore.QModelIndex)
     def open_pv_details(self, index: QtCore.QModelIndex, view: QtWidgets.QAbstractItemView) -> None:
-        print("open_pv_details")
         if not index.isValid():
             logger.warning("Invalid index passed to open_pv_details")
             return
+        data: Parameter | Setpoint | Readback
+        if isinstance(index.model(), PVBrowserFilterProxyModel):
+            source_model = index.model().sourceModel()
+            source_index = index.model().mapToSource(index)
+            data = source_model._data[source_index.row()]
+        elif isinstance(index.model(), PVTableModel):
+            data = index.model()._data[index.row()]
+        else:
+            raise TypeError("Invalid model type passed to open_pv_details")
+
+        # Get data via the client for alarm limits
+        epics_data: EpicsData
+        epics_data = self.client.cl.get(data.pv_name)
+
         pv_details = PVDetails(
-            pv_name="QUAD:LI21:401:EDES",
-            readback_name="QUAD:LI21:401:EACT",
-            description="This will be the description of the PV",
-            tolerance_abs=0.1,
-            tolerance_rel=0.01,
-            lolo=0.0,
-            low=0.1,
-            high=0.9,
-            hihi=1.0,
+            pv_name=data.pv_name,
+            readback_name=data.readback.pv_name if data.readback else None,
+            description=data.description,
+            tolerance_abs=data.abs_tolerance if isinstance(data, Parameter) else None,
+            tolerance_rel=data.rel_tolerance if isinstance(data, Parameter) else None,
+            lolo=epics_data.lower_alarm_limit,
+            low=epics_data.lower_warning_limit,
+            high=epics_data.upper_warning_limit,
+            hihi=epics_data.upper_alarm_limit,
             tags=None,
         )
         self.popup = PVDetailsPopup(pv_details)
