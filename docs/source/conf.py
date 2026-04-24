259069961ed9a19380b7d2cf45e12b5b9df6e771
tangkong <tangkongrobert@gmail.com>
2026-04-24 14:45:58 -0700

    DOC: add graphviz sphinx extension to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx_autodoc_typehints",
     "sphinx.ext.autosummary",
+    "sphinx.ext.graphviz",
     "sphinx.ext.inheritance_diagram",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
